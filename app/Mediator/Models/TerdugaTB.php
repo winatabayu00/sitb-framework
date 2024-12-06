1ef9241ead61229f3ce47231e61a23fad3051b40
ryzen500 <ahmadtsani145@gmail.com>
2024-12-06 22:11:44 +0700

    feat:list_terduga_tb

--- app/Mediator/Models/TerdugaTB.php
+++ app/Mediator/Models/TerdugaTB.php
@@ -13,6 +13,23 @@ class TerdugaTB extends Model implements HasMediatorLog
 
     protected $table = 'tb_terduga';
     protected $fillable = [
-
+        'person_id',
+        'no_sediaan',
+        'terduga_tb_id',
+        'terduga_ro_id',
+        'tipe_pasien_id',
+        'status_dm_id',
+        'status_hiv_id',
+        'imunisasi_bcg_id',
+        'status_tb'
     ];
+
+
+       /**
+     * Relasi ke model Pasien.
+     */
+    public function pasien()
+    {
+        return $this->belongsTo(Pasien::class, 'person_id', 'id_pasien'); // 'id' adalah primary key di tabel pasien_m
+    }
 }
